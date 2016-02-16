7d719d8046932b10ce70d69384597c89907365b9
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2016-02-16 06:37:10 -0700

    Added ubuntu setup files.

---
+++ ubuntu_setup_sudo.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Can only run in linux.
+if [ "$(uname)" == "Darwin" ]; then
+    echo "error: can not run script in Mac OS X"
+    exit 1
+fi
+
+# This must be run as sudo.
+if [ "$(whoami)" != "root" ]; then
+    echo "error: must run with sudo"
+    exit 1
+fi
+
+# Set up additional apt repos.
+apt-add-repository -y ppa:git-core/ppa
+add-apt-repository -y ppa:ubuntu-elisp
+apt-get update
+
+# Add latest git.
+apt-get install -y git
+
+# Add latest emacs.
+apt-get install -y emacs-snapshot
+
+# Add other packages.
+apt-get install -y synaptic
+
+exit 0
+
--- ubuntu_setup_user.sh
+++ ubuntu_setup_user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# make_link source destination
+# make_link source destination.
 function make_link {
     local source=$1
     local destination=$2
@@ -10,16 +10,11 @@ function make_link {
         echo "removed symbolic link $destination"
     fi
 
-    if [ -f $destination ]; then
-        rm $destination
+    if [ -e $destination ]; then
+        rm -rf $destination
         echo "removed file $destination"
     fi
 
-    if [ -d $destination ]; then
-        echo "error: $destination exists as a directory"
-        exit 1
-    fi
-    
     ln -s $source $destination
 
     if [ -L $destination ]; then
@@ -30,20 +25,49 @@ function make_link {
     fi
 }
 
+# Can only run in linux.
+if [ "$(uname)" == "Darwin" ]; then
+    echo "error: can not run script in Mac OS X"
+    exit 1
+fi
+
+# Check that git exists.
+if [ ! -e /usr/bin/git ]; then
+    echo "error: git not present, run ubuntu_setup_sudo.sh first"
+    exit 1
+fi
+
+# Make ~/tmp.
+if [ ! -d ~/tmp ]; then
+    mkdir ~/tmp
+    echo "created ~/tmp directory"
+fi
+
+# Link all of the directories and dot files.
 make_link /media/psf/Home/r                     ~/r
 make_link ~/r/os/linux/home/glenn/.bash_profile ~/.bash_profile
 make_link ~/r/os/linux/home/glenn/.bashrc       ~/.bashrc
 make_link ~/r/emacs.d                           ~/.emacs.d
-make_link /media/psf/Home/.emacs.d.elpa         ~/.emacsd.elpa
+make_link /media/psf/Home/.emacs.d.elpa         ~/.emacs.d.elpa
 make_link /media/psf/Home/.emacs.d.persistent   ~/.emacs.d.persistent
 make_link ~/r/git/gitconfig                     ~/.gitconfig
 make_link /media/psf/Home                       ~/mac
 
+# Get adobe source code pro fonts.
+git clone --depth 1 --branch release https://github.com/adobe-fonts/source-code-pro.git ~/.fonts/adobe-fonts/source-code-pro
+fc-cache -f -v ~/.fonts/adobe-fonts/source-code-pro
+echo "added adobe source code pro font"
+
+# Remove useless files.
 rm -f ~/.bash_logout ~/.profile
 echo "removed useless files"
 
+# Remove useless folders.
 rm -rf Documents Music Pictures Public Templates Videos
 echo "removed useless folders"
+
+echo
+echo "you must restart linux for fonts to take effect"
 
 exit 0
 
