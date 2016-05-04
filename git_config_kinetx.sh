6c0d2c86ba2980a0d72a32959cd7cbc7a3b419fa
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2016-05-04 08:07:24 -0700

    Added git_config_kinetx.sh.

---
+++ git_config_kinetx.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ ! -e .git ]]; then
+    echo "error: not in a git repository directory"
+    exit 1
+fi
+
+git config user.name  "Glenn Ehrlich"
+git config user.email "glenn.ehrlich@kinetx.com"
+
+exit 0
