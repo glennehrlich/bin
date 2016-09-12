1c6a8babd1e2b71faea287a6bddf8411212f7139
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2016-09-12 06:44:00 -0700

    Updated brew_emacs.sh.
    Also verified that it is getting most recent github HEAD.

---
+++ brew_emacs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+brew reinstall emacs --HEAD --with-cocoa --with-gnutls --with-imagemagick
+
+open /usr/local/Cellar/emacs
+
+echo
+echo "Remove Emacs.app from dock."
+
+echo
+echo "Drag /usr/local/Cellar/emacs/HEAD*/Emacs.app to dock."
