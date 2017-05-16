fd71ed6533bc5d3562a963706e21e32578e09983
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2017-05-15 21:38:50 -0700

    Added brew_mega_update.sh.

---
+++ brew_mega_update.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+ask_sudo() {
+  echo "This script needs the sudo password."
+  read -s -p 'Password:' sudo_password
+  echo
+  sudo --stdin --validate <<< "${sudo_password}" 2> /dev/null
+  until sudo -n true 2> /dev/null; do
+    read -s -p 'Password:' sudo_password
+    echo
+    sudo --stdin --validate <<< "${sudo_password}" 2> /dev/null
+  done
+}
+
+renew_sudo() {
+  sudo --stdin --validate <<< "${sudo_password}" 2> /dev/null
+}
+
+if pstree -p $PPID | grep -v grep | grep -q -i emacs; then
+    echo "error: you can not run this script in a shell within emacs (because emacs will be removed and installed)."
+    exit 1
+fi
+
+ask_sudo
+
+echo "------------------------------------------------------------"
+echo "pruining brew"
+brew prune
+
+echo "------------------------------------------------------------"
+echo "updating brew"
+brew update
+
+echo "------------------------------------------------------------"
+echo "upgrading brew"
+brew upgrade
+
+echo "------------------------------------------------------------"
+echo "updating emacs"
+brew uninstall --force emacs
+brew install emacs --HEAD --with-cocoa --with-gnutls --with-imagemagick@6 --with-librsvg --with-mailutils --with-modules
+dockutil --remove Emacs
+dockutil --add /usr/local/Cellar/emacs/HEAD*/Emacs.app --after Safari
+cd ~/.emacs.d ; make update_elpa ; make clean all ; make clean all ; make clean all
+
+for i in $(brew cask outdated --quiet); do 
+    echo "------------------------------------------------------------"
+    echo "fetching $i"
+    brew cask fetch $i
+    echo
+done    
+
+for i in $(brew cask outdated --quiet); do 
+    echo "------------------------------------------------------------"
+    echo "updating $i"
+    renew_sudo
+    brew cask install $i --force
+    echo
+done    
+
+echo "------------------------------------------------------------"
+echo "cleaning up brew"
+brew cask cleanup
