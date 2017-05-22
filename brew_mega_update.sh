649d16690b502bd40248be9d66b72a0687ffddde
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2017-05-22 12:23:51 -0700

    Removed redundant make cleans.

--- brew_mega_update.sh
+++ brew_mega_update.sh
@@ -41,7 +41,9 @@ brew uninstall --force emacs
 brew install emacs --HEAD --with-cocoa --with-gnutls --with-imagemagick@6 --with-librsvg --with-mailutils --with-modules
 dockutil --remove Emacs
 dockutil --add /usr/local/Cellar/emacs/HEAD*/Emacs.app --after Safari
-cd ~/.emacs.d ; make update_elpa ; make clean all ; make clean all ; make clean all
+cd ~/.emacs.d
+make update_elpa
+make clean all
 
 for i in $(brew cask outdated --quiet); do 
     echo "------------------------------------------------------------"
