0c2733aa158338a826b7110481694120d3f59bb1
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2017-10-21 16:19:55 -0700

    Removed obsolete repositories.

--- git_mega_repos.sh
+++ git_mega_repos.sh
@@ -1,25 +1,22 @@
-GIT_REPOS=(                                \
-  /gitlab/kinetx_forks/czml                \
-  /gitlab/kinetx_forks/mice                \
-  /gitlab/kinetx_forks/pypdevs             \
-  /gitlab/kinetx_forks/python-wrapper-main \
-                                           \
-  /gitlab/kinetx_glenn/design              \
-  /gitlab/kinetx_glenn/examples            \
-  /gitlab/kinetx_glenn/python              \
-  /gitlab/kinetx_glenn/python-devs         \
-  /gitlab/kinetx_glenn/resources           \
-  /gitlab/kinetx_glenn/sims                \
-  /gitlab/kinetx_glenn/util                \
-                                           \
-  /gitlab/kinetx_julia/julia_devs          \
-                                           \
-  /gitlab/kinetx_oneweb/vm                 \
-  /gitlab/kinetx_oneweb/vm_releases        \
-  /gitlab/kinetx_oneweb/work               \
-                                           \
-  /gitlab/kinetx_python/k                  \
-  /gitlab/kinetx_python/k_releases         \
-                                           \
-  ~/.emacs.d                               \
+GIT_REPOS=(                                  \
+  /gitlab/kinetx_forks/czml                  \
+  /gitlab/kinetx_forks/mice                  \
+  /gitlab/kinetx_forks/pypdevs               \
+  /gitlab/kinetx_forks/python-wrapper-main   \
+                                             \
+  /gitlab/kinetx_glenn/design                \
+  /gitlab/kinetx_glenn/examples              \
+  /gitlab/kinetx_glenn/python                \
+  /gitlab/kinetx_glenn/python-devs           \
+  /gitlab/kinetx_glenn/resources             \
+  /gitlab/kinetx_glenn/sims                  \
+  /gitlab/kinetx_glenn/util                  \
+  /gitlab/kinetx_glenn_private/notes         \
+                                             \
+  /gitlab/kinetx_python/k                    \
+  /gitlab/kinetx_python/k_releases           \
+                                             \
+  /gitlab/kinetx_virtual_machines/generic    \
+                                             \
+  ~/.emacs.d                                 \
 )
