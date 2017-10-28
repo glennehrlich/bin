999e0618eb516bd37fa0f77b7bc792532d2f084a
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2017-10-28 14:47:07 -0700

    Changed path to gitlab in git_mega_repos files.

--- git_mega_repos.sh
+++ git_mega_repos.sh
@@ -1,22 +1,24 @@
-GIT_REPOS=(                                  \
-  /gitlab/kinetx_forks/czml                  \
-  /gitlab/kinetx_forks/mice                  \
-  /gitlab/kinetx_forks/pypdevs               \
-  /gitlab/kinetx_forks/python-wrapper-main   \
-                                             \
-  /gitlab/kinetx_glenn/design                \
-  /gitlab/kinetx_glenn/examples              \
-  /gitlab/kinetx_glenn/python                \
-  /gitlab/kinetx_glenn/python-devs           \
-  /gitlab/kinetx_glenn/resources             \
-  /gitlab/kinetx_glenn/sims                  \
-  /gitlab/kinetx_glenn/util                  \
-  /gitlab/kinetx_glenn_private/notes         \
-                                             \
-  /gitlab/kinetx_python/k                    \
-  /gitlab/kinetx_python/k_releases           \
-                                             \
-  /gitlab/kinetx_virtual_machines/generic    \
-                                             \
-  ~/.emacs.d                                 \
+GIT_REPOS=(                                 \
+  ~/gitlab/kinetx_forks/czml                \
+  ~/gitlab/kinetx_forks/mice                \
+  ~/gitlab/kinetx_forks/pypdevs             \
+  ~/gitlab/kinetx_forks/python-wrapper-main \
+                                            \
+  ~/gitlab/kinetx_glenn/design              \
+  ~/gitlab/kinetx_glenn/examples            \
+  ~/gitlab/kinetx_glenn/python              \
+  ~/gitlab/kinetx_glenn/python-devs         \
+  ~/gitlab/kinetx_glenn/resources           \
+  ~/gitlab/kinetx_glenn/sims                \
+  ~/gitlab/kinetx_glenn/util                \
+  ~/gitlab/kinetx_glenn_private/notes       \
+                                            \
+  ~/gitlab/kinetx_python/k                  \
+  ~/gitlab/kinetx_python/k_releases         \
+                                            \
+  ~/gitlab/kinetx_virtual_machines/generic  \
+  ~/gitlab/kinetx_virtual_machines/kai2     \
+  ~/gitlab/kinetx_virtual_machines/kai3     \
+                                            \
+  ~/.emacs.d                                \
 )
--- git_mega_repos_OBSOLETE.sh
+++ git_mega_repos_OBSOLETE.sh
@@ -1,11 +1,11 @@
-GIT_REPOS=(                                  \
-  /gitlab/kinetx_julia/julia_devs            \
-                                             \
-  /gitlab/kinetx_oneweb/notes                \
-  /gitlab/kinetx_oneweb/vm                   \
-  /gitlab/kinetx_oneweb/vm_releases          \
-  /gitlab/kinetx_oneweb/work                 \
-  /gitlab/kinetx_oneweb_customer/vm_releases \
-  /gitlab/kinetx_oneweb_customer/work        \
-                                             \
+GIT_REPOS=(                                   \
+  ~/gitlab/kinetx_julia/julia_devs            \
+                                              \
+  ~/gitlab/kinetx_oneweb/notes                \
+  ~/gitlab/kinetx_oneweb/vm                   \
+  ~/gitlab/kinetx_oneweb/vm_releases          \
+  ~/gitlab/kinetx_oneweb/work                 \
+  ~/gitlab/kinetx_oneweb_customer/vm_releases \
+  ~/gitlab/kinetx_oneweb_customer/work        \
+                                              \
 )
