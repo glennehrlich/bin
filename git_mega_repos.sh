8e4c6d2952f94126a7f6a70366f6cb45427e8500
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2017-11-27 05:11:01 -0700

    Added sample_python_project to git_mega_repos.sh.

--- git_mega_repos.sh
+++ git_mega_repos.sh
@@ -1,25 +1,26 @@
-GIT_REPOS=(                                 \
-  ~/gitlab/kinetx_forks/czml                \
-  ~/gitlab/kinetx_forks/PythonPDEVS         \
-                                            \
-  ~/gitlab/kinetx_glenn/design              \
-  ~/gitlab/kinetx_glenn/examples            \
-  ~/gitlab/kinetx_glenn/python              \
-  ~/gitlab/kinetx_glenn/python-devs         \
-  ~/gitlab/kinetx_glenn/resources           \
-  ~/gitlab/kinetx_glenn/sims                \
-  ~/gitlab/kinetx_glenn/util                \
-                                            \
-  ~/gitlab/kinetx_glenn_private/notes       \
-                                            \
-  ~/gitlab/kinetx_python/k                  \
-  ~/gitlab/kinetx_python/k_releases         \
-                                            \
-  ~/gitlab/kinetx_virtual_machines/c        \
-  ~/gitlab/kinetx_virtual_machines/g        \
-  ~/gitlab/kinetx_virtual_machines/generic  \
-  ~/gitlab/kinetx_virtual_machines/kai2     \
-  ~/gitlab/kinetx_virtual_machines/kai3     \
-                                            \
-  ~/.emacs.d                                \
+GIT_REPOS=(                                   \
+  ~/gitlab/kinetx_forks/czml                  \
+  ~/gitlab/kinetx_forks/PythonPDEVS           \
+                                              \
+  ~/gitlab/kinetx_glenn/design                \
+  ~/gitlab/kinetx_glenn/examples              \
+  ~/gitlab/kinetx_glenn/python                \
+  ~/gitlab/kinetx_glenn/python-devs           \
+  ~/gitlab/kinetx_glenn/resources             \
+  ~/gitlab/kinetx_glenn/sample_python_project \
+  ~/gitlab/kinetx_glenn/sims                  \
+  ~/gitlab/kinetx_glenn/util                  \
+                                              \
+  ~/gitlab/kinetx_glenn_private/notes         \
+                                              \
+  ~/gitlab/kinetx_python/k                    \
+  ~/gitlab/kinetx_python/k_releases           \
+                                              \
+  ~/gitlab/kinetx_virtual_machines/c          \
+  ~/gitlab/kinetx_virtual_machines/g          \
+  ~/gitlab/kinetx_virtual_machines/generic    \
+  ~/gitlab/kinetx_virtual_machines/kai2       \
+  ~/gitlab/kinetx_virtual_machines/kai3       \
+                                              \
+  ~/.emacs.d                                  \
 )
