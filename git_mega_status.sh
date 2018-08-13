178e091fbb78c3c2a7a6bb7aed8ca1f0e797dc09
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2018-08-13 16:35:10 -0700

    Added printing list of unclean repos to git_mega_status.sh.
    The same change does not really make sense for git_mega_push.sh.

--- git_mega_status.sh
+++ git_mega_status.sh
@@ -10,14 +10,30 @@ function git_status {
     cd $dir
     git rev-parse --abbrev-ref HEAD # show current checked out branch
     if [[ -n $(git status --porcelain) ]]; then
-        echo "error: $dir is not clean"
+        echo "warning: $dir is not clean"
+        return 1
+    else
+        return 0
     fi
 }
 
+status=0
+unclean_repos=()
 for repo in ${GIT_REPOS[@]}; do
-    git_status $repo
+    if ! git_status $repo; then
+        status=1
+        unclean_repos+=($repo)
+    fi
 done
 
+echo
 echo "------------------------------------------------------------"
-echo "finished"
+if [ $status == 0 ]; then
+    echo "all repos are clean"
+else
+    echo "the following repos are not clean:"
+    for r in ${unclean_repos[@]}; do
+        echo "  $r"
+    done
+fi
 exit 0
