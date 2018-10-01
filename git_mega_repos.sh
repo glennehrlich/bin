b5597479ba35377915ffb80d9a30e609665a0902
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2018-10-01 15:27:58 -0700

    Added northstar repositories to git_mega_repos.sh.

--- git_mega_repos.sh
+++ git_mega_repos.sh
@@ -1,43 +1,49 @@
-GIT_REPOS=(                                    \
-  ~/gitlab/kinetx_bams/bar                     \
-  ~/gitlab/kinetx_bams/bar_manual_load         \
-  ~/gitlab/kinetx_bams/bar_server              \
-  ~/gitlab/kinetx_bams/installer               \
-  ~/gitlab/kinetx_bams/kickstart               \
-  ~/gitlab/kinetx_bams/notes                   \
-                                               \
-  ~/gitlab/kinetx_forks/czml                   \
-  ~/gitlab/kinetx_forks/PythonPDEVS            \
-                                               \
-  ~/gitlab/kinetx_glenn/design                 \
-  ~/gitlab/kinetx_glenn/examples               \
-  ~/gitlab/kinetx_glenn/python                 \
-  ~/gitlab/kinetx_glenn/python-devs            \
-  ~/gitlab/kinetx_glenn/resources              \
-  ~/gitlab/kinetx_glenn/sample_python_project  \
-  ~/gitlab/kinetx_glenn/sims                   \
-  ~/gitlab/kinetx_glenn/spacetrack             \
-  ~/gitlab/kinetx_glenn/util                   \
-                                               \
-  ~/gitlab/kinetx_glenn_private/notes          \
-                                               \
-  ~/gitlab/kinetx_python/k                     \
-  ~/gitlab/kinetx_python/k_releases            \
-                                               \
-  ~/gitlab/kinetx_studies/vulcan               \
-                                               \
-  ~/gitlab/kinetx_virtual_machines/bams        \
-  ~/gitlab/kinetx_virtual_machines/bams_build  \
-  ~/gitlab/kinetx_virtual_machines/bams_server \
-  ~/gitlab/kinetx_virtual_machines/bar         \
-  ~/gitlab/kinetx_virtual_machines/c           \
-  ~/gitlab/kinetx_virtual_machines/centos      \
-  ~/gitlab/kinetx_virtual_machines/g           \
-  ~/gitlab/kinetx_virtual_machines/j           \
-  ~/gitlab/kinetx_virtual_machines/generic     \
-  ~/gitlab/kinetx_virtual_machines/kai2        \
-  ~/gitlab/kinetx_virtual_machines/kai3        \
-  ~/gitlab/kinetx_virtual_machines/redhat      \
-                                               \
-  ~/.emacs.d                                   \
+GIT_REPOS=(                                            \
+  ~/gitlab/kinetx_bams/bar                             \
+  ~/gitlab/kinetx_bams/bar_manual_load                 \
+  ~/gitlab/kinetx_bams/bar_server                      \
+  ~/gitlab/kinetx_bams/installer                       \
+  ~/gitlab/kinetx_bams/kickstart                       \
+  ~/gitlab/kinetx_bams/notes                           \
+                                                       \
+  ~/gitlab/kinetx_forks/czml                           \
+  ~/gitlab/kinetx_forks/PythonPDEVS                    \
+                                                       \
+  ~/gitlab/kinetx_glenn/design                         \
+  ~/gitlab/kinetx_glenn/examples                       \
+  ~/gitlab/kinetx_glenn/python                         \
+  ~/gitlab/kinetx_glenn/python-devs                    \
+  ~/gitlab/kinetx_glenn/resources                      \
+  ~/gitlab/kinetx_glenn/sample_python_project          \
+  ~/gitlab/kinetx_glenn/sims                           \
+  ~/gitlab/kinetx_glenn/spacetrack                     \
+  ~/gitlab/kinetx_glenn/util                           \
+                                                       \
+  ~/gitlab/kinetx_glenn_private/notes                  \
+                                                       \
+  ~/gitlab/kinetx_northstar/notes                      \
+  ~/gitlab/kinetx_northstar/virtual_machines/northstar \
+                                                       \
+  ~/gitlab/kinetx_python/k                             \
+  ~/gitlab/kinetx_python/k_releases                    \
+                                                       \
+  ~/gitlab/kinetx_studies/vulcan                       \
+                                                       \
+  ~/gitlab/kinetx_virtual_machines/bams                \
+  ~/gitlab/kinetx_virtual_machines/bams_build          \
+  ~/gitlab/kinetx_virtual_machines/bams_server         \
+  ~/gitlab/kinetx_virtual_machines/bar                 \
+  ~/gitlab/kinetx_virtual_machines/bar_test_client     \
+  ~/gitlab/kinetx_virtual_machines/c                   \
+  ~/gitlab/kinetx_virtual_machines/centos              \
+  ~/gitlab/kinetx_virtual_machines/g                   \
+  ~/gitlab/kinetx_virtual_machines/j                   \
+  ~/gitlab/kinetx_virtual_machines/generic             \
+  ~/gitlab/kinetx_virtual_machines/kai2                \
+  ~/gitlab/kinetx_virtual_machines/kai3                \
+  ~/gitlab/kinetx_virtual_machines/nfs_private_network \
+  ~/gitlab/kinetx_virtual_machines/redhat              \
+  ~/gitlab/kinetx_virtual_machines/viasat              \
+                                                       \
+  ~/.emacs.d                                           \
 )
