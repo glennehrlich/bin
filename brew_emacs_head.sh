2766fc49d9279be92825e46b0e672af59aa63475
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2018-11-19 05:58:15 -0700

    Removed obsolete build option in brew_emacs*.sh.

--- brew_emacs_head.sh
+++ brew_emacs_head.sh
@@ -2,7 +2,7 @@
 
 echo "brewing with emacs HEAD"
 brew uninstall --force emacs
-brew install emacs --HEAD --with-cocoa --with-gnutls --with-imagemagick@6 --with-librsvg --with-mailutils --with-modules
+brew install emacs --HEAD --with-cocoa --with-imagemagick@6 --with-librsvg --with-mailutils --with-modules
 dockutil --remove Emacs
 dockutil --add /usr/local/Cellar/emacs/HEAD*/Emacs.app --after Safari
 cd ~/.emacs.d
--- brew_emacs_release.sh
+++ brew_emacs_release.sh
@@ -2,6 +2,6 @@
 
 echo "brewing with emacs release"
 brew uninstall --force emacs
-brew install emacs --with-cocoa --with-gnutls --with-imagemagick@6 --with-librsvg --with-mailutils --with-modules
+brew install emacs --with-cocoa --with-imagemagick@6 --with-librsvg --with-mailutils --with-modules
 dockutil --remove Emacs
 dockutil --add /usr/local/Cellar/emacs/HEAD*/Emacs.app --after Safari
