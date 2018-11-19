c29f97ef38166d05f8432f8ab75def74b9201570
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2018-11-19 05:58:04 -0700

    Updated git_mega_repos.sh.

--- git_mega_repos.sh
+++ git_mega_repos.sh
@@ -1,32 +1,20 @@
 GIT_REPOS=(                                                 \
+  ~/gitlab/kinetx_bams/bams                                 \
   ~/gitlab/kinetx_bams/bar                                  \
   ~/gitlab/kinetx_bams/bar_manual_load                      \
   ~/gitlab/kinetx_bams/bar_server                           \
   ~/gitlab/kinetx_bams/installer                            \
   ~/gitlab/kinetx_bams/kickstart                            \
   ~/gitlab/kinetx_bams/notes                                \
-  ~/gitlab/kinetx_bams/virtual_machines/bams                \
+  ~/gitlab/kinetx_bams/sdramBit                             \
   ~/gitlab/kinetx_bams/virtual_machines/bams_build          \
   ~/gitlab/kinetx_bams/virtual_machines/bams_server         \
   ~/gitlab/kinetx_bams/virtual_machines/bar                 \
   ~/gitlab/kinetx_bams/virtual_machines/bar_test_client     \
-  ~/gitlab/kinetx_bams/virtual_machines/centos              \
-  ~/gitlab/kinetx_bams/virtual_machines/nfs_private_network \
-  ~/gitlab/kinetx_bams/virtual_machines/redhat              \
   ~/gitlab/kinetx_bams/virtual_machines/viasat              \
                                                             \
   ~/gitlab/kinetx_forks/czml                                \
   ~/gitlab/kinetx_forks/PythonPDEVS                         \
-                                                            \
-  ~/gitlab/kinetx_glenn/design                              \
-  ~/gitlab/kinetx_glenn/examples                            \
-  ~/gitlab/kinetx_glenn/python                              \
-  ~/gitlab/kinetx_glenn/python-devs                         \
-  ~/gitlab/kinetx_glenn/resources                           \
-  ~/gitlab/kinetx_glenn/sample_python_project               \
-  ~/gitlab/kinetx_glenn/sims                                \
-  ~/gitlab/kinetx_glenn/spacetrack                          \
-  ~/gitlab/kinetx_glenn/util                                \
                                                             \
   ~/gitlab/kinetx_glenn_private/notes                       \
                                                             \
@@ -35,6 +23,16 @@ GIT_REPOS=(                                                 \
                                                             \
   ~/gitlab/kinetx_python/k                                  \
   ~/gitlab/kinetx_python/k_releases                         \
+                                                            \
+  ~/gitlab/kinetx_simulation/design                         \
+  ~/gitlab/kinetx_simulation/examples                       \
+  ~/gitlab/kinetx_simulation/python                         \
+  ~/gitlab/kinetx_simulation/python-devs                    \
+  ~/gitlab/kinetx_simulation/resources                      \
+  ~/gitlab/kinetx_simulation/sample_python_project          \
+  ~/gitlab/kinetx_simulation/sims                           \
+  ~/gitlab/kinetx_simulation/spacetrack                     \
+  ~/gitlab/kinetx_simulation/util                           \
                                                             \
   ~/gitlab/kinetx_studies/vulcan                            \
                                                             \
