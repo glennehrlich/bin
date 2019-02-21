7c7044801ac9c1fae31d48d7c05b76d471f9b4df
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2019-02-21 10:26:55 -0700

    Removed obsolete repos from git_mega_repos.sh.

--- git_mega_repos.sh
+++ git_mega_repos.sh
@@ -1,41 +1,24 @@
-GIT_REPOS=(                                                 \
-  ~/gitlab/kinetx_bams/bams                                 \
-  ~/gitlab/kinetx_bams/notes                                \
-  ~/gitlab/kinetx_bams/virtual_machines/bams_build          \
-  ~/gitlab/kinetx_bams/virtual_machines/bams_server         \
-  ~/gitlab/kinetx_bams/virtual_machines/bar                 \
-  ~/gitlab/kinetx_bams/virtual_machines/bar_test_client     \
-  ~/gitlab/kinetx_bams/virtual_machines/viasat              \
-                                                            \
-  ~/gitlab/kinetx_forks/czml                                \
-  ~/gitlab/kinetx_forks/PythonPDEVS                         \
-                                                            \
-  ~/gitlab/kinetx_glenn_private/notes                       \
-                                                            \
-  ~/gitlab/kinetx_northstar/notes                           \
-  ~/gitlab/kinetx_northstar/virtual_machines/northstar      \
-                                                            \
-  ~/gitlab/kinetx_python/k                                  \
-  ~/gitlab/kinetx_python/k_releases                         \
-                                                            \
-  ~/gitlab/kinetx_simulation/design                         \
-  ~/gitlab/kinetx_simulation/examples                       \
-  ~/gitlab/kinetx_simulation/python                         \
-  ~/gitlab/kinetx_simulation/python-devs                    \
-  ~/gitlab/kinetx_simulation/resources                      \
-  ~/gitlab/kinetx_simulation/sample_python_project          \
-  ~/gitlab/kinetx_simulation/sims                           \
-  ~/gitlab/kinetx_simulation/spacetrack                     \
-  ~/gitlab/kinetx_simulation/util                           \
-                                                            \
-  ~/gitlab/kinetx_studies/vulcan                            \
-                                                            \
-  ~/gitlab/kinetx_virtual_machines/c                        \
-  ~/gitlab/kinetx_virtual_machines/g                        \
-  ~/gitlab/kinetx_virtual_machines/j                        \
-  ~/gitlab/kinetx_virtual_machines/generic                  \
-  ~/gitlab/kinetx_virtual_machines/kai2                     \
-  ~/gitlab/kinetx_virtual_machines/kai3                     \
-                                                            \
-  ~/.emacs.d                                                \
+GIT_REPOS=(                                                  \
+  ~/gitlab/kinetx_bams/bams                                  \
+  ~/gitlab/kinetx_bams/notes                                 \
+                                                             \
+  ~/gitlab/kinetx_forks/czml                                 \
+  ~/gitlab/kinetx_forks/PythonPDEVS                          \
+                                                             \
+  ~/gitlab/kinetx_glenn_private/notes                        \
+                                                             \
+  ~/gitlab/kinetx_northstar/notes                            \
+  ~/gitlab/kinetx_northstar/virtual_machines/northstar       \
+                                                             \
+  ~/gitlab/kinetx_simulation/design                          \
+  ~/gitlab/kinetx_simulation/examples                        \
+  ~/gitlab/kinetx_simulation/python                          \
+  ~/gitlab/kinetx_simulation/python-devs                     \
+  ~/gitlab/kinetx_simulation/resources                       \
+  ~/gitlab/kinetx_simulation/sample_python_project           \
+  ~/gitlab/kinetx_simulation/sims                            \
+  ~/gitlab/kinetx_simulation/spacetrack                      \
+  ~/gitlab/kinetx_simulation/util                            \
+                                                             \
+  ~/.emacs.d                                                 \
 )
