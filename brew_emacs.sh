3caa53cd18eed327913ec6d82b7db0771c00ba8d
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2019-04-13 14:01:06 -0700

    Allow emacs to build with jansson json library in brew_emacs.sh.

--- brew_emacs.sh
+++ brew_emacs.sh
@@ -2,7 +2,7 @@
 
 echo "brewing with emacs-plus HEAD"
 brew uninstall --force emacs-plus
-brew install emacs-plus --HEAD --with-cocoa --with-imagemagick@6 --with-librsvg --with-mailutils --with-modules --without-spacemacs-icon
+brew install emacs-plus --HEAD --with-cocoa --with-imagemagick@6 --with-librsvg --with-mailutils --with-modules --with-jansson --without-spacemacs-icon
 dockutil --remove Emacs
 dockutil --add /usr/local/opt/emacs-plus/Emacs.app --after Safari
 cd ~/.emacs.d
