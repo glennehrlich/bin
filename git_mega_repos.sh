d8a1bfd5c60a2f1a90ed02db43bf18cc79e4cfe5
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2019-05-16 13:17:42 -0700

    Recursive git status functions.

--- git_mega_repos.sh
+++ git_mega_repos.sh
@@ -1,30 +1,10 @@
-GIT_REPOS=(                                                  \
-  ~/gitlab/kinetx_bams/bams                                  \
-  ~/gitlab/kinetx_bams/notes                                 \
-                                                             \
-  ~/gitlab/kinetx_forks/czml                                 \
-  ~/gitlab/kinetx_forks/PythonPDEVS                          \
-                                                             \
-  ~/gitlab/kinetx_glenn_private/notes                        \
-                                                             \
-  ~/gitlab/kinetx_northstar/notes                            \
-  ~/gitlab/kinetx_northstar/virtual_machines/northstar       \
-                                                             \
-  ~/gitlab/kinetx_simulation/design                          \
-  ~/gitlab/kinetx_simulation/examples                        \
-  ~/gitlab/kinetx_simulation/python                          \
-  ~/gitlab/kinetx_simulation/python-devs                     \
-  ~/gitlab/kinetx_simulation/resources                       \
-  ~/gitlab/kinetx_simulation/sample_python_project           \
-  ~/gitlab/kinetx_simulation/sims                            \
-  ~/gitlab/kinetx_simulation/spacetrack                      \
-  ~/gitlab/kinetx_simulation/util                            \
-                                                             \
-  ~/.emacs.d                                                 \
-  ~/.ssh                                                     \
-  ~/bin                                                      \
-  ~/dot-files                                                \
-  ~/notes                                                    \
-  ~/notes-personal                                           \
-  ~/todo                                                     \
+GIT_REPOS=(        \
+  ~/.emacs.d       \
+  ~/.ssh           \
+  ~/bin            \
+  ~/dot-files      \
+  ~/gitlab         \
+  ~/notes          \
+  ~/notes-personal \
+  ~/todo           \
 )
--- git_mega_status.sh
+++ git_mega_status.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
+# Necessary to run stuff in pipe in current process (while read dir; do)
+shopt -s lastpipe
+
 source git_mega_repos.sh
 
 function git_status {
     local dir=$1
 
-    echo "------------------------------------------------------------"
-    echo "checking $dir"
+    # echo "------------------------------------------------------------"
+    # echo "checking $dir"
     cd $dir
-    git rev-parse --abbrev-ref HEAD # show current checked out branch
+    # git rev-parse --abbrev-ref HEAD # show current checked out branch
     if [[ -n $(git status --porcelain) ]]; then
-        echo "warning: $dir is not clean"
+        # echo "warning: $dir is not clean"
         return 1
     else
         return 0
@@ -20,13 +23,17 @@ function git_status {
 exit_code=0
 
 status=0
+declare -x unclean_repos
 unclean_repos=()
-for repo in ${GIT_REPOS[@]}; do
-    if ! git_status $repo; then
-        status=1
-        unclean_repos+=($repo)
-        exit_code=1
-    fi
+for gr in ${GIT_REPOS[@]}; do
+    find $gr -name .git -type d -prune | while read dir; do
+        repo=$(dirname $dir)
+        if ! git_status $repo; then
+            status=1
+            unclean_repos+=($repo)
+            exit_code=1
+        fi
+    done
 done
 
 echo
@@ -36,7 +43,8 @@ if [ $status == 0 ]; then
 else
     echo "The following repos are not clean:"
     for r in ${unclean_repos[@]}; do
-        echo "$r"
+        branch=$(git rev-parse --abbrev-ref HEAD)
+        echo "$r on $branch"
     done
 fi
 
