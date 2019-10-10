e81cb0ca6910112d42cc024b0e6da17423808f16
Glenn Ehrlich <glenn.ehrlich@kinetx.com>
2019-10-10 14:15:36 -0700

    Update vagrant_setup_redhat.sh
    Needed to update due to reorganized files on mac.

--- vagrant_setup_redhat.sh
+++ vagrant_setup_redhat.sh
@@ -7,12 +7,12 @@ function make_link {
     local source=$1
     local destination=$2
 
-    if [ -L $destination ]; then
+    if [[ -L $destination ]]; then
         rm $destination
         echo "removed symbolic link $destination"
     fi
 
-    if [ -e $destination ]; then
+    if [[ -e $destination ]]; then
         rm -rf $destination
         echo "removed file $destination"
     fi
@@ -20,7 +20,7 @@ function make_link {
     ln -s $source $destination
     chown -h vagrant:vagrant $destination
 
-    if [ -L $destination ]; then
+    if [[ -L $destination ]]; then
         echo "created symbolic link $destination to $source"
     else
         echo "error: could not create symbolic link $destination to $source"
@@ -30,37 +30,31 @@ function make_link {
 
 # This should be run only as root from the vagrant provisioning
 # process.
-if [ "$(whoami)" != "root" ]; then
+if [[ "$(whoami)" != "root" ]]; then
     echo "error: must run with root from vagrant provisioning"
     exit 1
 fi
 
 # Can only run in linux.
-if [ "$(uname)" == "Darwin" ]; then
+if [[ "$(uname)" == "Darwin" ]]; then
     echo "error: can not run script in Mac OS X"
     exit 1
 fi
 
 # Check that git exists.
-if [ ! -e /usr/bin/git ]; then
+if [[ ! -e /usr/bin/git ]]; then
     echo "error: git not present"
     exit 1
 fi
 
 # Check that /home/vagrant/host exists.
-if [ ! -e /home/vagrant/host ]; then
+if [[ ! -e /home/vagrant/host ]]; then
     echo "error: /home/vagrant/host not present"
     exit 1
 fi
 
-# Check that /home/vagrant/r exists.
-if [ ! -e /home/vagrant/r ]; then
-    echo "error: /home/vagrant/r not present"
-    exit 1
-fi
-
 # Make ~/tmp.
-if [ ! -d /home/vagrant/tmp ]; then
+if [[ ! -d /home/vagrant/tmp ]]; then
     mkdir /home/vagrant/tmp
     chown vagrant:vagrant /home/vagrant/tmp
     echo "created /home/vagrant/tmp directory"
@@ -71,20 +65,20 @@ mv /home/vagrant/.bashrc /home/vagrant/.bashrc.original
 chown vagrant:vagrant /home/vagrant/.bashrc.original
 
 # Link all of the directories and dot files.
-make_link /home/vagrant/host/.emacs.d.elpa                       /home/vagrant/.emacs.d.elpa
-make_link /home/vagrant/host/r/os/linux/home/glenn/.bash_profile /home/vagrant/.bash_profile
-make_link /home/vagrant/r/emacs.d                                /home/vagrant/.emacs.d
-make_link /home/vagrant/r/git/gitconfig                          /home/vagrant/.gitconfig
-make_link /home/vagrant/r/os/vagrant/bams/.bashrc                /home/vagrant/.bashrc
+make_link /home/vagrant/dot-files/bams/.bash_profile        /home/vagrant/.bash_profile
+make_link /home/vagrant/dot-files/bams/.bashrc              /home/vagrant/.bashrc
+make_link /home/vagrant/dot-files/ubuntu-vagrant/.gitconfig /home/vagrant/.gitconfig
 
-# Create the emacs persistent directories.
-( cd /home/vagrant/.emacs.d ; su vagrant -c "make create_persistent_dirs" )
-chown -R vagrant:vagrant /home/vagrant/.emacs.d.persistent
+# Set up emacs
+( cd /home/vagrant ;          su vagrant -c "git clone https://github.com/glennehrlich/emacs.d .emacs.d" )
+( cd /home/vagrant ;          su vagrant -c "mkdir .emacs.d.elpa" )
+( cd /home/vagrant/.emacs.d ; su vagrant -c "EMACS=/usr/local/bin/emacs make create_persistent_dirs" )
+( cd /home/vagrant/.emacs.d ; su vagrant -c "EMACS=/usr/local/bin/emacs make update_elpa" )
+( cd /home/vagrant/.emacs.d ; su vagrant -c "EMACS=/usr/local/bin/emacs make -k clean all" ) # make -k because vterm will not build in redhat
 rm -rf /home/vagrant/.emacs.d.persistent.old
 
 # Get adobe source code pro fonts.
-# git clone --depth 1 --branch release https://github.com/adobe-fonts/source-code-pro.git /home/vagrant/.fonts/adobe-fonts/source-code-pro
-git clone --depth 1 --branch release git@github.com:adobe-fonts/source-code-pro.git /home/vagrant/.fonts/adobe-fonts/source-code-pro
+git clone --depth 1 --branch release https://github.com/adobe-fonts/source-code-pro.git /home/vagrant/.fonts/adobe-fonts/source-code-pro
 fc-cache -f -v /home/vagrant/.fonts/adobe-fonts/source-code-pro
 chown -R vagrant:vagrant /home/vagrant/.fonts
 echo "added adobe source code pro font"
@@ -98,4 +92,3 @@ rm -rf Documents Music Pictures Public Templates Videos
 echo "removed useless folders"
 
 exit 0
-
