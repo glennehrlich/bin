da982ec7c1e0b0602c1d3416cd72563edfce5181
Glenn Ehrlich <glenn.ehrlich@kinetx.com>
2019-10-28 12:16:36 -0700

    Added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Run the vm command bin/deploy.sh in the current git repository.
+
+# Test if within a git repository.
+if ! git rev-parse --is-inside-work-tree  > /dev/null 2>&1; then
+    echo "error: not inside a git repository"
+    exit 1
+fi
+
+git_repo=`git rev-parse --show-toplevel`
+
+# cd to the toplevel of the git repository.
+cd $git_repo
+
+if [ ! -e bin ]; then
+    echo "error: there is no bin directory underneath $git_repo"
+    exit 1
+fi
+
+if [ ! -e bin/deploy.sh ]; then
+    echo "error: there is no deploy.sh in $git_repo/bin"
+    exit 1
+fi
+
+echo "executing $git_repo/bin/deploy.sh"
+bin/deploy.sh
+
+exit 0
