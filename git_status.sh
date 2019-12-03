73a7bd7aaa4dfbde1a2303eadcdde1c5b67952d7
Glenn Ehrlich <glenn.ehrlich@kinetx.com>
2019-12-03 10:42:09 -0700

    Update git_status.sh with changed directories

--- git_status.sh
+++ git_status.sh
@@ -8,7 +8,7 @@ mgitstatus $options ~/.ssh           1
 mgitstatus $options ~/bin            1
 mgitstatus $options ~/dot-files      1
 mgitstatus $options ~/gitlab         5
+mgitstatus $options ~/gitlab-archive 5
 mgitstatus $options ~/notes          1
 mgitstatus $options ~/notes-personal 1
-mgitstatus $options ~/repos          2
 mgitstatus $options ~/todo           1
