818069e33821e6d4799a363828f701d2db96b662
Glenn Ehrlich <glenn.ehrlich@kinetx.com>
2020-08-08 14:09:11 -0700

    Update brew_install_emacs.sh to emacs28

--- brew_install_emacs.sh
+++ brew_install_emacs.sh
@@ -2,10 +2,10 @@
 
 brew tap d12frosted/emacs-plus
 
-brew uninstall --force emacs-plus
-brew install emacs-plus --without-spacemacs-icon
+brew uninstall --force emacs-plus@28
+brew install emacs-plus@28 --with-jansson
 dockutil --remove Emacs
-dockutil --add /usr/local/opt/emacs-plus/Emacs.app --after Safari
+dockutil --add /usr/local/opt/emacs-plus@28/Emacs.app --after Safari
 cd ~/.emacs.d
 make update_elpa
 make clean all
