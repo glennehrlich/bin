57c45f5e75e8de725d1c1d523906df1293fcce20
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2020-10-29 11:40:09 -0700

    setup_ubuntu_wsl.sh in process

---
+++ setup_ubuntu_wsl.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# the idea here is to pull all the different stuff from provision
+# scripts and stuff i have in ~/bin
+
+
+
+
+# these are the apt-get installs i did in wsl2
+
+   26  sudo apt-get install emacs
+   94  sudo apt-get install bash-completion
+  109  sudo apt-get install xterm
+  111  sudo apt-get install xterm
+  114  sudo apt-get install synaptic
+  119  sudo apt install emacs27
+  152  sudo apt-get install libvterm
+  153  sudo apt-get install cmake
+  155  sudo apt-get install libtool-bin
+  156  sudo apt-get install libvterm-dev
+  158  sudo apt-get install ripgrep
+  193  sudo apt-get uninstall emacs27
+  199  sudo apt --fix-borken install
+  200  sudo apt-get install emacs-snapshot emacs-snapshot-el emacs-snapshot-common
+  203  sudo apt-get install -y emacs-snapshot emacs-snapshot-el
+  204  sudo apt-get install -y emacs-snapshot emacs-snapshot-el emacs-snapshot-common
+  206  sudo apt-get install -y emacs-snapshot emacs-snapshot-el emacs-snapshot-common
+  210  sudo apt-get install -y emacs-snapshot emacs-snapshot-el emacs-snapshot-common
+  215  sudo apt-get install emacs-snapshot emacs-snapshot-el
+  268  sudo apt-get install dbus-x11
