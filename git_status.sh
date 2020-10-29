9190d91ed08837e02eb589e83195e8f5033de94c
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2020-10-29 11:48:46 -0700

    Minor tweaks to git_status.sh

--- git_status.sh
+++ git_status.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-options="-e --depth=1"
+# options="-e --depth=0"
+options="--depth=0"
 
 mgitstatus $options ~/.emacs.d
-mgitstatus $options ~/.aws
 mgitstatus $options ~/.ssh
 mgitstatus $options ~/bin
 mgitstatus $options ~/dot-files
-# mgitstatus $options ~/gitlab
+mgitstatus $options ~/gitlab
 # mgitstatus $options ~/gitlab-archive
 mgitstatus $options ~/notes
 mgitstatus $options ~/notes-personal
