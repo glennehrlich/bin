e8b19548cbfd848c7aa50b7176ef20526924ab8a
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2020-10-31 21:19:51 -0700

    Additional draft work on setup_ubuntu_wsl.sh

--- setup_ubuntu_wsl.sh
+++ setup_ubuntu_wsl.sh
@@ -28,3 +28,17 @@
   210  sudo apt-get install -y emacs-snapshot emacs-snapshot-el emacs-snapshot-common
   215  sudo apt-get install emacs-snapshot emacs-snapshot-el
   268  sudo apt-get install dbus-x11
+
+
+mkdir ~/github
+mkdir ~/gitlab
+mkdir ~/tmp
+
+# install ripgrep
+cd ~/tmp
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep_12.1.1_amd64.deb
+sudo dpkg -i ripgrep_12.1.1_amd64.deb 
+
+# install multi-git-status
+cd ~/github
+git clone git@github.com:fboender/multi-git-status.git
