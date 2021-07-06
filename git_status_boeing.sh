ab7444e80e8aefdcc830452b5687d394a421169b
Glenn Ehrlich <glenn.ehrich2@boeing.com>
2021-07-06 08:32:25 -0700

    Update git_status_boeing.sh to ignore aga repos

--- git_status_boeing.sh
+++ git_status_boeing.sh
@@ -8,7 +8,9 @@ mgitstatus $options ~/.ssh
 mgitstatus $options ~/bin
 mgitstatus $options ~/dot-files
 mgitstatus $options ~/g
-mgitstatus $options ~/git
+
+# mgitstatus $options ~/git/aga # 2021-07-06: not doing aga progjects at this time
+
 mgitstatus $options ~/github
 mgitstatus $options ~/notes
 mgitstatus $options ~/todo
