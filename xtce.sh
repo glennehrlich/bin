87974e6ea951e6f145da0c8e81efb175a4fb60a5
Glenn Ehrlich <glenn.ehrich2@boeing.com>
2022-06-03 15:32:10 -0700

    Change location of xtcetools in xtce.sh

--- xtce.sh
+++ xtce.sh
@@ -1,2 +1,2 @@
-java -jar ~/git-reference/xtcetools/target/xtcetools-1.1.6-SNAPSHOT-jar-with-dependencies.jar &
+java -jar ~/git-other/xtcetools/target/xtcetools-1.1.6-SNAPSHOT-jar-with-dependencies.jar &
 
