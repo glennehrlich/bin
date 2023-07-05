17f98f1c8c0dd8127abd661c2623eb7e473197ff
Glenn Ehrlich <glenn.ehrich2@boeing.com>
2023-07-05 14:26:47 -0700

    Add starting redis

--- start_bc2.sh
+++ start_bc2.sh
@@ -5,6 +5,10 @@ rm -f ~/bc2/usr/etc/log/*
 
 cd ~/bc2/usr/bin
 
+echo "starting redis servier"
+docker run --name redis --detach --tty --rm --publish 11500:11500 -v ~/bc2:/usr/data/storage bc2-docker-sandbox-sdte.artifactory-sdteob.web.boeing.com/bc2/redis-server:6.2.6-R1 /usr/data/redis.conf --dir /usr/data/storage
+sleep 5
+
 echo "starting event distributer"
 ./message_distributor_proxy --name event_distributor_proxy1 &
 sleep 5
