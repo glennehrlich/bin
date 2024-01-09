8fdf23e0ed50dac0b7cb60841915b2544629c206
Glenn Ehrlich <glenn.ehrich2@boeing.com>
2024-01-09 16:12:40 -0700

    Better bc2 stopping coordination

--- stop_bc2.sh
+++ stop_bc2.sh
@@ -8,7 +8,11 @@ pkill -f parameter
 pkill -f sdbservice
 docker kill mss_sim_sv030
 pkill -f message
+
+echo "sleeping for 5 seconds to allow services to cleanly stop before stopping redis"
+sleep 5
 docker kill redis
-echo "sleeping for 5 seconds"
+
+echo "sleeping for 5 seconds after stopping redis before stopping etcd-server"
 sleep 5
 docker kill etcd-server
